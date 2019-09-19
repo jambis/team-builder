1d771c80e658e629a86c539e6f2082b7af66a5e7
jambis <imjaamees@gmail.com>
2019-09-18 21:03:37 -0500

    Added Team Members h1 title

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -12,7 +12,11 @@ function App() {
   return (
     <div style={{ margin: "20px auto", width: "100%" }}>
       <Form addMember={addMember} />
-
+      {team.length > 0 ? (
+        <div style={{ textAlign: "center" }}>
+          <h1>Team Members</h1>
+        </div>
+      ) : null}
       {team.map(member => {
         return <Member member={member} />;
       })}
